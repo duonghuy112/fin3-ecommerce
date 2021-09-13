1a11ac47331c36e399fa4d9df7a5278cc6263282
duonghuy1123 <duoonghuy1123@gmail.com>
2021-09-13 11:50:03 +0700

    fin3-ecommerce-spring-boot start & config 13092021

---
+++ fin3-ecommerce/src/main/java/com/nguyenduonghuy/fin3ecommerce/Fin3EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenduonghuy.fin3ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fin3EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fin3EcommerceApplication.class, args);
+	}
+
+}
