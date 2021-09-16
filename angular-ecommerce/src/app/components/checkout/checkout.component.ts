74068698aa1f447d357117c1b021d2a2d748f345
duonghuy1123 <duoonghuy1123@gmail.com>
2021-09-16 22:28:46 +0700

    checkout layout 160921

---
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup!: FormGroup;
+
+  totalQuantity: number = 0;
+  totalPrice: number = 0;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        country: [''],
+        city: [''],
+        district: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        cardName: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+  }
+
+  onSubmit() {
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+}
